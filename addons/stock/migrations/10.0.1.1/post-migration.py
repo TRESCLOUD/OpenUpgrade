341f8b3d0897c855d042288351ca366fd3bec901
Andres Calle <andres.calle@trescloud.com>
2017-05-25 19:18:33 -0500

    [10.0][Stock] Improved structure as suggested by contributors

--- addons/stock/migrations/10.0.1.1/post-migration.py
+++ addons/stock/migrations/10.0.1.1/post-migration.py
@@ -1,31 +1,29 @@
 # -*- coding: utf-8 -*-
-# © 2017 Trescloud <http://trescloud.com>
+# Copyright 2017 Trescloud <http://trescloud.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from openupgradelib import openupgrade
 
-@openupgrade.migrate(use_env=True)
-def migrate(env, version):
-    update_picking_type_id(env)
-    update_ordered_qty(env)
-    populate_stock_scrap(env)
-    
 def update_picking_type_id(env):
-    '''
-    Updates picking_type_id, defaulting to some value, guessing by looking at source
+    """Updates picking_type_id, defaulting to some value, guessing by looking at source
     and destination locations, and warehouse.  
     :param env: enviroment variable (self)
-    '''
-    
+    """
+    #load xml data to be used for filling in missing info
+    xml_stock_picking_type_internal = env.ref("stock.picking_type_internal")
+    xml_stock_picking_type_out = env.ref("stock.picking_type_out")
+    xml_stock_picking_type_in = env.ref("stock.picking_type_in")
+    xml_stock_picking_type_manufacturing = env.ref("mrp.picking_type_manufacturing")
+    #verify for each procument rule to set
     procurement_rules_to_set = env['procurement.rule'].search([])
-    
     for procurement_rule in procurement_rules_to_set:
         if not procurement_rule.picking_type_id:
-            
             picking_type_id = False
             env.cr.execute(
-                """
+                '''
                 SELECT id from stock_picking_type 
-                WHERE warehouse_id = %s AND default_location_dest_id = %s AND default_location_src_id = %s""" % (
+                WHERE warehouse_id = %s AND 
+                      default_location_dest_id = %s AND 
+                      default_location_src_id = %s''' % (
                     procurement_rule.warehouse_id, 
                     procurement_rule.location_id,
                     procurement_rule.location_src_id,
@@ -34,63 +32,50 @@ def update_picking_type_id(env):
             picking_type_ids = cr.fetchone()
             if picking_type_ids:
                 picking_type_id = picking_type_ids[0]
-            
             if not picking_type_id: #fallback values when everything else fails
                 if procurement_rule.action == 'buy':
                     #location_src_id is not considered as is not mandatory in this case
-                    xml_record = env.ref("stock.picking_type_in")
-                    if xml_record:
-                        picking_type_id = xml_record.id
+                    if xml_stock_picking_type_in:
+                        picking_type_id = xml_stock_picking_type_in.id
                 elif procurement_rule.action == 'move':
                     if procurement_rule.location_id and procurement_rule.location_id.usage == 'customer':
                         #special case when is a delivarable to a customer location
-                        xml_record = env.ref("stock.picking_type_out")
-                        if xml_record:
-                            picking_type_id = xml_record.id
+                        if xml_stock_picking_type_out:
+                            picking_type_id = xml_stock_picking_type_out.id
                     else:
-                        xml_record = env.ref("stock.picking_type_internal")
-                        if xml_record:
-                            picking_type_id = xml_record.id
+                        if xml_stock_picking_type_internal:
+                            picking_type_id = xml_stock_picking_type_internal.id
                 elif procurement_rule.action == 'manufacture':
-                    xml_record = env.ref("mrp.picking_type_manufacturing")
-                    if xml_record:
-                        picking_type_id = xml_record.id
-        
+                    if xml_stock_picking_type_manufacturing:
+                        picking_type_id = xml_stock_picking_type_manufacturing.id
             procurement_rule.write({'picking_type_id': picking_type_id})
 
 def update_ordered_qty(env):
-    '''
-    Set as default the value of field ordered_qty from the value of field product_uom_qty  
+    """ Set the value of new field ordered_qty as:
+      - for stock moves the value of field product_uom_qty
+      - for stock_pack_operation the value of product_qty  
     :param env: enviroment variable (self)
-    '''
-    #stock        / stock.move               / ordered_qty (float)           : NEW
-    # TODO: post-migration. Default to product_uom_qty
+    """
     env.cr.execute(
-        """
+        '''
         UPDATE stock_move SET ordered_qty = product_uom_qty
-        """)
-    
-    #stock        / stock.pack.operation     / ordered_qty (float)           : NEW
-    # TODO: post-migration. Default to product_uom_qty
+        ''')
     env.cr.execute(
-        """
+        '''
         UPDATE stock_pack_operation SET ordered_qty = product_qty
-        """)
+        ''')
     
 def populate_stock_scrap(env):
-    '''
-    Fills up new object "stock.scrap" based on moves with destination scrap  
+    """ Fills up new object "stock.scrap" based on moves with destination scrap  
     :param env: enviroment variable (self)
-    '''
-        
+    """
     env.cr.execute(
-        """
+        '''
         SELECT id from stock_location 
         WHERE scrap_location is True
-        """
+        '''
         )
     scrap_location_ids = env.cr.fetchone()
-    
     #do not call stock_scrap.create as it will create a duplicated stock move, use SQL instead
     #field package_id not set as no value is defined    
     env.cr.execute(
@@ -102,3 +87,8 @@ def populate_stock_scrap(env):
                 OR location_dest_id IN %s AND product_uom_qty >= 0.0 AND state = 'done'
         ''',(scrap_location_ids,scrap_location_ids))
     
+@openupgrade.migrate(use_env=True)
+def migrate(env, version):
+    update_picking_type_id(env)
+    update_ordered_qty(env)
+    populate_stock_scrap(env)
