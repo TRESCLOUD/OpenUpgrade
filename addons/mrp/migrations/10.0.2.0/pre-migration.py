b766f9f4bb8d7a3253d60da17e0198e048c88f6e
Patricio Rangles <patricio.rangles@trescloud.com>
2017-11-16 18:04:26 -0500

    [FIX][mrp]:
    try en un execute no funciona, se cambia la forma de verificacion

--- addons/mrp/migrations/10.0.2.0/pre-migration.py
+++ addons/mrp/migrations/10.0.2.0/pre-migration.py
@@ -2,17 +2,17 @@
 # © 2017 Therp BV <http://therp.nl>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from openupgradelib import openupgrade
-import logging
-
-_logger = logging.getLogger(__name__)
 
 @openupgrade.migrate(use_env=False)
 def migrate(cr, version):
     # create an xmlid for mail.bounce.alias is it exists
-    try:
-        cr.execute(
+    cr.execute(
+        """SELECT * FROM information_schema.tables 
+        WHERE table_schema = 'public' 
+        AND table_name = 'mrp_workorder'"""
+    )
+    record = cr.fetchall()
+    if record:
+            cr.execute(
             """DROP VIEW mrp_workorder"""
-        )
-    except:
-        _logger.error('Error al borrar la vista mrp_workorder')
-    
+        )    
